9ee51b396a2d755edafd53b97aee77c7297cd071
Kristofer Jürgenstein <50505081413@tptlive.ee>
2025-02-04 11:44:45 +0200

    Added a npm package

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import api from './api/axios'
 import { Button } from "@/components/ui/button"
+import TinderCard from 'react-tinder-card'
 import './App.css'
 
 function App() {
@@ -18,11 +19,21 @@ function App() {
       })
   }, [])
 
+  const onSwipe = (direction : string) => {
+    console.log('You swiped: ' + direction)
+  }
+
+  const onCardLeftScreen = (myIdentifier : string) => {
+    console.log(myIdentifier + ' left the screen')
+  }
+
   return (
     <div className="App">
       <h1 className='text-3xl font-bold underline'>Vite + React + Go</h1>
       <Button>Click me</Button>
       <p>API Status: {health}</p>
+
+      <TinderCard onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['up', 'down']}>Hello, World!</TinderCard>
     </div>
   )
 }
