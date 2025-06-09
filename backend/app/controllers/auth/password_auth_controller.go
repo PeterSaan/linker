958c8de607a7e866274b1c3616e0ddc7ac287058
PatrickLapik <patrick.lapimaa@tptlive.ee>
2025-06-09 08:12:26 +0300

    Password auth controller

--- backend/app/controllers/auth/password_auth_controller.go
+++ backend/app/controllers/auth/password_auth_controller.go
@@ -9,74 +9,74 @@ import (
 )
 
 func PasswordRegister(ctx *gin.Context) {
-    var formData structs.PasswordRegiserData
+	var formData structs.PasswordRegiserData
 
-    if err := ctx.ShouldBindJSON(&formData); err != nil {
-        ctx.JSON(http.StatusBadRequest, gin.H{
-            "error": err.Error(),
-        })
-        return
-    }
+	if err := ctx.ShouldBindJSON(&formData); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
 
-    if formData.Password != formData.PasswordConfirm {
-        ctx.JSON(http.StatusBadRequest, gin.H{
-            "error": "Both password fields must match",
-        })
-        return
-    }
+	if formData.Password != formData.PasswordConfirm {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "Both password fields must match",
+		})
+		return
+	}
 
-    user, err := services.PasswordRegisterUser(formData) 
-    if err != nil {
-        ctx.JSON(http.StatusBadRequest, gin.H{
-            "error": err.Error(),
-        })
-        return
-    }
+	user, err := services.PasswordRegisterUser(formData)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
 
-    tokenString, err := services.GenerateToken(&user)
-    if err != nil {
-        ctx.JSON(http.StatusBadRequest, gin.H{
-            "error": err.Error(),
-        })
-        return
-    }
+	tokenString, err := services.GenerateToken(&user)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
 
-    services.SetTokenCookie(ctx, tokenString)
+	services.SetTokenCookie(ctx, tokenString)
 }
 
 func PasswordLogin(ctx *gin.Context) {
-    var formData structs.PasswordLoginData
+	var formData structs.PasswordLoginData
 
-    if err := ctx.ShouldBindJSON(&formData); err != nil {
-        ctx.JSON(http.StatusBadRequest, gin.H{
-            "error": err.Error(),
-        })
-        return
-    }
+	if err := ctx.ShouldBindJSON(&formData); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
 
-    user, err := services.PasswordGetUser(formData)
-    if err != nil {
-        ctx.JSON(http.StatusBadRequest, gin.H{
-            "error": err.Error(),
-        })
-        return
-    }
+	user, err := services.PasswordGetUser(formData)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
 
-    tokenString, err := services.GenerateToken(&user)
-    if err != nil {
-        ctx.JSON(http.StatusBadRequest, gin.H{
-            "error": err.Error(),
-        })
-        return
-    }
+	tokenString, err := services.GenerateToken(&user)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
 
-    services.SetTokenCookie(ctx, tokenString)
+	services.SetTokenCookie(ctx, tokenString)
 }
 
 func Logout(ctx *gin.Context) {
-    ctx.SetCookie("linker_token", "", -1, "/", "localhost", false, true)
-    ctx.JSON(http.StatusOK, gin.H{
-        "success": true,
-        "message": "Logged out successfully!",
-    })
+	ctx.SetCookie("linker_token", "", -1, "/", "localhost", false, true)
+	ctx.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"message": "Logged out successfully!",
+	})
 }
